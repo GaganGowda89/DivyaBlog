33cd67327290862f2e39053fb73ad9a7b6e00d79
Gagan Gowda <gagan.nms@gmail.com>
2022-09-13 18:21:01 +0530

    Final commit V2.0

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -21,10 +21,10 @@ const Categories = () => {
         </Link>
       ))}
 
-    <h3 className="text-xl mb-8 font-semibold border-b pb-4">About Us</h3>
-    <p className="text-ls text-amber-600 pb-12"> Hi! We are two happy birds <strong>Divya</strong> and <strong>Gagan</strong> trying to add our little bit of life work in here. We do research in different fields, mainly in computer science and microbiology domain. Hence this platform covers about those specific topics. Nevertheless we happen to do many more things like, travel, lifestyle, healthy diet, food and lots more. A few bit of interesting blogs about these can make this blog tastier. </p>
-    <h3 className="text-xl mb-8 font-semibold border-b pb-4">Follow Us</h3>
-    <p className="text-ls pb-4 mb-8">  © Instagram  📸 : <a className="underline text-blue-600 hover:text-blue-900 visited:text-purple-600" href="https://www.instagram.com/divgan2702/"> Divgan2702 </a></p>
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">About Us</h3>
+      <p className="text-ls text-amber-600 pb-12"> Hi! We are two happy birds <strong>Divya</strong> and <strong>Gagan</strong> trying to add our little bit of life work in here. We do research in different fields, mainly in computer science and microbiology domain. Hence this platform covers about those specific topics. Nevertheless we happen to do many more things like, travel, lifestyle, healthy diet, food and lots more. A few bit of interesting blogs about these can make this blog tastier. </p>
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Follow Us</h3>
+      <p className="text-ls pb-4 mb-8">  © Instagram  📸 : <a className="underline text-blue-600 hover:text-blue-900 visited:text-purple-600" href="https://www.instagram.com/divgan2702/"> Divgan2702 </a></p>
     </div>
   );
 };
