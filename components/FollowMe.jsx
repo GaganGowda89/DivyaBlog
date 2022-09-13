27bef98f5456799e7dd937d99479afe496c34ba2
Gagan Gowda <gagan.nms@gmail.com>
2022-09-13 16:07:21 +0530

    Fixed space error by  ESLINT

--- components/FollowMe.jsx
+++ components/FollowMe.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const FollowMe = () => {
-  return (
+
+return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">About Us</h3>
       <p className="text-ls text-amber-600 pb-12"> Hi! We are two happy birds <strong>Divya</strong> and <strong>Gagan</strong> trying to add our little bit of life work in here. We do research in different fields, mainly in computer science and microbiology domain. Hence this platform covers about those specific topics. Nevertheless we happen to do many more things like, travel, lifestyle, healthy diet, food and lots more. A few bit of interesting blogs about these can make this blog tastier. </p>
